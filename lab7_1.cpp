26ea46f909d0847fccc3d7660ae58991d4e7df7d
pawitya <bollboll35142@gmail.com>
2024-12-06 14:12:37 +0700

    using namespace std;
    int main(){
       float a  =  20.0;
       float b = 30.0;
       float c = 4.0;
       float output;
    
       output = (a + b)/c;
       cout << "output: " << output << endl;
       return 0;
    }

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
-
-    outpot = a - b/c;
-    count << output: << output << \n;
-    return 0;
+int main(){
+   float a  =  20.0;
+   float b = 30.0;
+   float c = 4.0;
+   float output;
+   
+   
+   output = (a + b)/c;
+   cout << "output: " << output << endl;
+   return 0;
 }
